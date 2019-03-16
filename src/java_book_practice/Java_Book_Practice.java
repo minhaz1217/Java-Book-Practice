783ac347df1eef9b714df0f13da1d7de61ee2f3a
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-03-17 01:43:30 +0600

    initial commit

---
+++ src/java_book_practice/Java_Book_Practice.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_book_practice;
+
+/**
+ *
+ * @author Minhaz
+ */
+public class Java_Book_Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Book name is: Introduction to Java Programming");
+    }
+    
+}
